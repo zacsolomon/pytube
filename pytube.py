60c8c477c0ec54e878bf15263b585496ed6106c0
Zac Solomon <zacdsolomon@gmail.com>
2018-10-06 21:50:34 -0700

    second commit

--- pytube.py
+++ pytube.py
@@ -7,22 +7,22 @@ from subprocess import call
 
 # Add functionality to auto rename metadata and add to iTunes
  
-#def main () :
-#
-#	
-#		if sys.argv[1] == "-a":
-#			command = 'pytube ' + sys.argv[2];
-#			call(command.split(), shell=False);
-#
-#		elif sys.argv[1] == "-v":
-#			command = 'pytube ' + sys.argv[2];
-#			call(command.split(), shell=False);
-#		
-#		else :
-#			sys.exit("usage: " + sys.argv[0] + " [-a,-v,-av] <link>\n -a = audio\n -v = video\n -a+v = audio + video\n")
-#
-#if ( __name__ == "__main__" ) :
-#	main();
+def main () :
+
+	
+		if sys.argv[1] == "-a":
+			command = 'pytube ' + sys.argv[2];
+			call(command.split(), shell=False);
+
+		elif sys.argv[1] == "-v":
+			command = 'pytube ' + sys.argv[2];
+			call(command.split(), shell=False);
+		
+		else :
+			sys.exit("usage: " + sys.argv[0] + " [-a,-v,-av] <link>\n -a = audio\n -v = video\n -a+v = audio + video\n")
+
+if ( __name__ == "__main__" ) :
+	main();
 
 
 
